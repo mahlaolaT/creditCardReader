bc6a60a7f808ebb09fffef331fb516c3e502cb32
Thamsanqa Mahlaola <mahlaola.thamsanqa@gmail.com>
2024-09-20 16:05:18 +0200

    Adding Bloc/Cubit state management

---
+++ lib/bloc/card_cubit.dart
@@ -0,0 +1,50 @@
+import 'package:card_reader/cards.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'card_state.dart';
+
+class CardCubit extends Cubit<CardState> {
+  CardCubit() : super(CardInitial());
+
+  void validateCard(String cardNumber) {
+    emit(CardLoading());
+    // Simple validation logic for demonstration
+    if (cardNumber.length == 16) {
+      emit(CardValid(cardNumber));
+    } else {
+      emit(CardInvalid("Card number must be 16 digits long"));
+    }
+  }
+
+  saveCard(
+    String cardNumber,
+    String countryIssued,
+    String cardType,
+    String expiryDate,
+  ) async {
+    Box<Cards> box = await Hive.openBox('cards');
+    box.put(
+      cardNumber,
+      Cards(
+        cardNumber: int.parse(cardNumber),
+        countryIssued: countryIssued,
+        cardType: cardType,
+        expiryDate: expiryDate,
+      ),
+    );
+  }
+
+  savedCards() async {
+    Box<Cards> box = await Hive.openBox('cards');
+    List<Cards> cards = box.values.toList();
+    emit( cards.isNotEmpty ? CardSaved(cards) : CardInitial());
+  }
+
+  removeCard(int index) async {
+    Box<Cards> box = await Hive.openBox('cards');
+    List<Cards> cards = box.values.toList();
+    cards.removeAt(index);
+    box.delete(index);
+    emit(CardSaved(cards));
+  }
+}
--- lib/cards_screen.dart
+++ lib/cards_screen.dart
@@ -1,102 +1,84 @@
 import 'package:card_reader/add_card_screen.dart';
+import 'package:card_reader/bloc/card_cubit.dart';
+import 'package:card_reader/card_type.dart';
 import 'package:card_reader/cards.dart';
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/adapters.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc/card_state.dart';
 
 class CardsScreen extends StatelessWidget {
   const CardsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    CardCubit cardCubit = context.read<CardCubit>()..savedCards();
     return Scaffold(
-      body: ValueListenableBuilder(
-        valueListenable: Hive.box<Cards>('cards').listenable(),
-        builder: (context, Box<Cards> box, _) {
-          if (box.values.isEmpty) {
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(8.0),
-                    child: const Text(
-                      "Looks like you haven't added any cards yet.",
+      body: BlocBuilder<CardCubit, CardState>(builder: (context, state) {
+        if (state is CardSaved) {
+          return Center(
+            child: ListView.separated(
+              itemCount: state.cards.length,
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                Cards? card = state.cards[index];
+                return Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 15.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(8.0),
                     ),
                   ),
-                ],
-              ),
-            );
-          } else {
-            return Center(
-              child: ListView.separated(
-                itemCount: box.values.length,
-                shrinkWrap: true,
-                itemBuilder: (context, index) {
-                  Cards? card = box.getAt(index);
-                  return Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 15.0),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey),
-                      borderRadius: const BorderRadius.all(
-                        Radius.circular(8.0),
-                      ),
+                  child: ListTile(
+                    leading: CardType.getCardTypeIcon(CardType.fromDisplayName(card.cardType!.toLowerCase())),
+                    title: Text(card.cardNumber.toString()),
+                    subtitle: Text('Expires ${card.expiryDate}'),
+                    trailing: TextButton(
+                      child: const Text('REMOVE'),
+                      onPressed: () => cardCubit.removeCard(index),
                     ),
-                    child: ListTile(
-                      leading: _getCardTypeIcon(card!.cardType!),
-                      title: Text(card.cardNumber.toString()),
-                      subtitle: Text('Expires ${card.expiryDate}'),
-                      trailing: TextButton(
-                        child: const Text('REMOVE'),
-                        onPressed: () => box.deleteAt(index),
-                      ),
-                    ),
-                  );
-                },
-                separatorBuilder: (context, i) {
-                  return const SizedBox(height: 12);
-                },
+                  ),
+                );
+              },
+              separatorBuilder: (context, i) {
+                return const SizedBox(height: 12);
+              },
+            ),
+          );
+        }
+
+        return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(8.0),
+                child: const Text(
+                  "Looks like you haven't added any cards yet.",
+                ),
               ),
-            );
-          }
-        },
-      ),
+            ],
+          ),
+        );
+      }),
       bottomNavigationBar: Container(
         padding: const EdgeInsets.symmetric(horizontal: 48.0),
         child: TextButton(
           child: const Text("ADD CARD"),
           onPressed: () => Navigator.of(context).push(
             MaterialPageRoute(
-              builder: (context) => const AddCardScreen(),
+              builder: (context) => BlocProvider.value(
+                value: cardCubit,
+                child: const AddCardScreen(),
+              ),
             ),
           ),
         ),
       ),
     );
   }
-
-  Image _getCardTypeIcon(String cardType) {
-    switch (cardType.toLowerCase()) {
-      case 'mastercard':
-        return Image.asset(
-          "assets/images/mastercard.png",
-          width: 42,
-          height: 42,
-        );
-
-      case 'visa':
-        return Image.asset(
-          "assets/images/visa.png",
-          width: 42,
-          height: 42,
-        );
-
-      default:
-        return Image.asset(
-          "assets/images/unknown.png",
-          width: 42,
-          height: 42,
-        );
-    }
-  }
 }
+
+
